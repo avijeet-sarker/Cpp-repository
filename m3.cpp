11c7b8c6461c9be0ca3d1620ef653361e8651092
avijeet-sarker <avijeetsarker0@gmail.com>
2025-05-13 01:31:01 +0600

    m3.cpp

--- m3.cpp
+++ m3.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 int main(){
 int age = 25;
-cout<< age;
+char grade = 'A';
+float pi = 3.14f;
+bool issafe= true;
+double price = 100.99;
+cout<< age<< endl;
+cout<< sizeof(age)<< endl;
+cout << grade << endl;
+cout << pi << endl;
+cout << issafe << endl; // true-> 1 & false -> 0
+cout << price << endl;
 return 0;
 }
