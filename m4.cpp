e0edd9ca5d68f3c42927513873705fdc2f7ed0c9
avijeet-sarker <avijeetsarker0@gmail.com>
2025-05-14 11:46:27 +0600

    m4.cpp

---
+++ m4.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char grade ='a'; //97
+    int value = grade ;
+    double price = 100.99;
+    int newprice = (int)price;
+cout<< value<< endl ;
+cout << newprice;
+return 0;
+}
