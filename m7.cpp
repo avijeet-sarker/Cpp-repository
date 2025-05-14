354db65af7fcd7280235ca20b8b6e1eb00ad9afe
avijeet-sarker <avijeetsarker0@gmail.com>
2025-05-14 13:10:25 +0600

    m7.cpp

--- m7.cpp
+++ m7.cpp
@@ -5,5 +5,9 @@ int main (){
     cout << (3>5)<<endl;
     cout << (3<=3)<<endl;
     cout << (3!=5)<<endl;
+    cout << !(3<5)<<endl;
+    cout <<( (3<5) ||(3<1)) <<endl;
+     cout <<( (3<5) &&(3<1)) <<endl;
+      cout <<( (3<5) &&(3>1)) <<endl;
     return 0;
 }
