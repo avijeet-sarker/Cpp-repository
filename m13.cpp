47aa0fe20c9c2e008cf3f425d339919b989d04fe
avijeet-sarker <avijeetsarker0@gmail.com>
2025-05-15 00:29:01 +0600

    m13.cpp

---
+++ m13.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n= -76;
+    if(n>=0){
+        cout<<"n is positive\n";
+    }
+    else{
+ cout<<"n is negative\n";
+    }
+return 0;
+}
