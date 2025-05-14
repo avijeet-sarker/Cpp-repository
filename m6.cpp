686e38409b919d2b73a2fb193025045628b928f1
avijeet-sarker <avijeetsarker0@gmail.com>
2025-05-14 12:57:12 +0600

    m6.cpp

---
+++ m6.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a= 11 , b=5 ;
+    double c=2;
+    int ans = (a/(double)b);
+cout<<"sum="<<(a+b)<<endl;
+cout<<"difference="<<(a-b)<<endl;
+cout<<"product="<<(a*b)<<endl;
+cout<<"division="<<(a/b)<<endl;
+cout<<"modulo="<<(a%b)<<endl;
+cout<<"division="<<(a/c)<<endl;
+cout<<"diff="<<(a/(double)b)<<endl;
+cout<<ans<<endl;
+return 0;
+}
