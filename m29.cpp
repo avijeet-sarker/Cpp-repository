746e778473ccf927689cb4e20959f4c0e2c55af3
avijeet-sarker <avijeetsarker0@gmail.com>
2025-05-16 01:19:44 +0600

    m28.cpp

---
+++ m29.cpp
@@ -0,0 +1,14 @@
+# include<iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=5;i++){
+        cout<<"*****"<<endl;
+    
+    }
+    int m=5;
+     for(int i=1;i<=m;i++){
+        cout<<"*";
+    
+    }
+return 0;
+}
