3a86e409c7f20cb6588ff675afbdd6d44b7438cb
avijeet-sarker <avijeetsarker0@gmail.com>
2025-05-18 17:55:10 +0600

    m4.cpp

--- m4.cpp
+++ m4.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main(){
     char grade ='a'; //97
-    int value = grade ;
+    int value = grade ;//implicit
     double price = 100.99;
-    int newprice = (int)price;
+    int newprice = (int)price;//explicit
 cout<< value<< endl ;
 cout << newprice;
 return 0;
